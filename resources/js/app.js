71795c56f3dff85ff223b7efa30ed2e440104792
MLiban10 <2151574@my.ipleiria.pt>
2019-01-06 21:15:42 +0000

    server

--- resources/js/app.js
+++ resources/js/app.js
@@ -22,18 +22,18 @@ axios.defaults.headers.common['Authorization'] = sessionStorage.getItem('access_
 axios.defaults.headers.common['Content-Type'] = "application/json";
 
 Vue.component('master', require('./components/Master.vue'));
-Vue.component('item-list', require('./components/ItemList.vue'));
+Vue.component('item-list', require('./components/itemList.vue'));
 
-Vue.component('login', require('./components/auth/Login.vue'));
+Vue.component('login', require('./components/auth/login.vue'));
 Vue.component('registerWorker', require('./components/auth/RegisterWorker.vue'));
-Vue.component('profile', require('./components/auth/Profile.vue'));
+Vue.component('profile', require('./components/auth/profile.vue'));
 Vue.component('edit-user', require('./components/auth/UserEdit.vue'));
 
-Vue.component('dashboard', require('./components/Dashboard.vue'));
+Vue.component('dashboard', require('./components/dashboard.vue'));
 Vue.component('dashboardWaiter', require('./components/waiter/dashboardWaiter.vue'));
 Vue.component('dashboardCook', require('./components/cook/DashboardCook.vue'));
 Vue.component('dashboardCashier', require('./components/cashier/DashboardCashier.vue'));
-Vue.component('userManage', require('./components/manager/UserManage.vue'));
+Vue.component('userManage', require('./components/manager/userManage.vue'));
 Vue.component('menuItemsManage', require('./components/manager/MenuItemsManage.vue'));
 Vue.component('edit-menuitem', require('./components/manager/MenuItemEdit.vue'));
 Vue.component('add-menuitem', require('./components/manager/MenuItemAdd.vue'));
